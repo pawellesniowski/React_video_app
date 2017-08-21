a775483c9bd9ce32d1fe88373314e608af0e1871
Pawel <pawel.html@gmail.com>
2017-08-21 22:08:52 +0200

    added search componenet

---
+++ src/components/search_bar.js
@@ -0,0 +1,10 @@
+// to translate jsx to javascript we need to import React.
+import React from 'react';
+
+
+const SearchBar = () =>{
+    return <input />
+}
+
+//export code:
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom'; // deals wit browser/DOM
 
+import SearchBar from './components/search_bar.js';
+
+const API_KEY = 'AIzaSyCm0k_4aAm7vSzzZ4RWod34meZ0f8OFiQI';
+
 //create a new component. this component should produce some Html:
-const App = () => <div>Hi!</div>;
+const App = () => { 
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
+}
+
 
 
 // Take above component and generate Html and put it on the page / in the DOM
