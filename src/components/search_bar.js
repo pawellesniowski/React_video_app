47ad2934cce9cc562b9ef7d8c83cbc52e5b83421
Pawel <pawel.html@gmail.com>
2017-08-21 22:40:04 +0200

    Handling User Events

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,16 @@
 // to translate jsx to javascript we need to import React.
-import React from 'react';
+import React, {Component} from 'react';
 
 
-const SearchBar = () =>{
-    return <input />
+class SearchBar extends Component{
+    // every React component that we create, 
+    //that is class based has to have a render method:
+    render() { // syntax for method in the classes
+        return <input onChange={(event)=>console.log(event.target.value)}/>;
+    }
+    // onInputChange(event){
+    //     console.dir(event);
+    // }
 }
 
 //export code:
